968ba4c589de2549a0f7fd4e3ce2874b1ddb2b6c
airadier <airadier@f2109389-8d3c-484c-a695-9c476c4d84ce>
2009-02-08 20:43:12 +0000

    Forgot to commit notesmanager

---
+++ src/notesmanager.py
@@ -0,0 +1,35 @@
+import os
+
+class NotesManager():
+    
+    def __init__(self, file):
+        f = open(file, "r")
+        signature = f.read(4).lower()
+        self.notes = []
+        if signature != '%xpr':
+            self.pdf_file = file
+            f.close()
+            return
+        
+        self.pdf_file = os.path.splitext(file)[0] + ".pdf"
+        
+        current_note = u""
+        
+        lines = f.readlines()[1:]
+        for line in lines:
+            if line.startswith("-----"):
+                self.notes.append(current_note)
+                current_note = u""
+            else:
+                current_note = current_note + line
+        f.close()
+            
+    def get_pdf_file(self):
+        return self.pdf_file
+    
+    def get_notes(self, page):
+        if page < len(self.notes):
+            return self.notes[page]
+        else:
+            return ""
+                          
