2b59c12de36766bb83aea7ab451e7cc59fd9cab4
airadier <airadier@f2109389-8d3c-484c-a695-9c476c4d84ce>
2009-02-20 10:09:14 +0000

    Added GPLv3 License

--- remote-src/dotnet-remote/xpressent-remote/SelectDevice.Designer.cs
+++ remote-src/dotnet-remote/xpressent-remote/SelectDevice.Designer.cs
@@ -1,4 +1,22 @@
-﻿namespace xpressent_remote
+﻿/*
+Copyright 2009, Alvaro J. Iradier
+This file is part of xPressent (Windows Mobile Remote).
+
+xPressent is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+xPressent is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with xPressent.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace xpressent_remote
 {
 	partial class SelectDevice
 	{
